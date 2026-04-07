2695fd9fd9a57a31964f8ebda539f474706f7a08
kasarshravani04 <shravanikasar5@gmail.com>
2026-04-07 13:48:10 +0530

    feat:import fonts & title logo

---
+++ src/components/layout/MainHeader.jsx
@@ -0,0 +1,13 @@
+import { NavLink } from "react-router-dom";
+
+const MainHeader = () => {
+  return (
+    <header>
+        <div>
+            
+        </div>
+    </header>
+  )
+}
+
+export default MainHeader;
